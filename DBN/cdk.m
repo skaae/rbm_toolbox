cfbc5b110b941a471cf50dfc1c911124c9ed3e34
Søren Sønderby <tropehjelm@gmail.com>
2014-06-23 21:29:09 +0200

    fix cdk documentation

--- DBN/cdk.m
+++ DBN/cdk.m
@@ -1,15 +1,17 @@
-function [dW,db,dc,curr_err] = cdk(rbm,v0,k )
+function [dw,db,dc,curr_err] = cdk(rbm,v0,k )
 %CDK performs contrastive divergence n_times
 %   Contrastive divergence sampling.
 %   see "A practical guide to training restricted Boltzmann machines" section
 %   3.4.
 %   INPUTS:
 %     rbm: a rbm struct
-%     v1 ; the initial state of the hidden units
-%     n  :  number of contrastive divergence steps
+%     v0        ; the initial state of the hidden units
+%     k         :  number of contrastive divergence steps
 %   OUTPUTS
-%     dW, db,dc: weight and visible/hidden biases
-%     curr_err : current error
+%    dw         : w weights chainge
+%    db         : bias of visible layer weight change
+%    dc         : bias of hidden layer weight change
+%    curr_err   : current squared error
 % See also
 %   Hinton, G. (2002). Training Products of Experts by Minimizing Contrastive
 %   Divergence. Neural Compu- tation, 14, 1771?1800.
@@ -31,7 +33,7 @@ hk = hid_deterministic;
 positive_phase = h0' * v0;
 negative_phase = hk' * vk;
 
-dW = positive_phase - negative_phase;
+dw = positive_phase - negative_phase;
 db =  sum(v0 - vk)';
 dc =  sum(h0 - hk)';
 curr_err = sum(sum((v0 - vk) .^ 2));
