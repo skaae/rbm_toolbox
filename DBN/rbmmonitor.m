087e9d34e7b1da77fa54cb6ee485bb231956cc7d
Søren Sønderby <tropehjelm@gmail.com>
2014-07-15 13:51:37 +0200

    fix train and val error reporting

--- DBN/rbmmonitor.m
+++ DBN/rbmmonitor.m
@@ -9,15 +9,21 @@ function [ perf,rbm ] = rbmmonitor(rbm,x,opts,val_samples,epoch)
 %
 %   Returns a string for dispaly
 % Copyright Søren Sønderby July 2014
-if mod(epoch,opts.test_interval) == 0 && ~isempty(opts.x_val)
+if mod(epoch,opts.test_interval) == 0
     % a) lassRBM calculate validation and training error
     % b) generativeRBM  calculate ratio of free energy fe_val /fe_train, if
     %    much lower than 1 we are overfitting
     if rbm.classRBM
         rbm.train_error(end+1) = rbmcalcerr(rbm,x,opts.y_train);
-        rbm.val_error(end+1) = rbmcalcerr(rbm,opts.x_val,opts.y_val);
-        perf = sprintf('  | Tr: %5f - Val: %5f' ,...
-            rbm.train_error(end),rbm.val_error(end));
+        if ~isempty(opts.x_val)
+            rbm.val_error(end+1) = rbmcalcerr(rbm,opts.x_val,opts.y_val);
+            val_err = num2str(rbm.val_error(end+1));
+        else
+            val_err = 'NA';
+        end
+        
+        perf = sprintf('  | Tr: %5f - Val: %s' ,...
+            rbm.train_error(end),val_err);
     else
         warning('Remimpelent rbmfreeenergy');
         e_val = rbmfreeenergy(rbm,opts.x_val,opts.y_val);
